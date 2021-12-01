18cd3cb1c600dfdadfb38ee538ac1191ae461d15
Kevin Leutzinger <thedookmaster@gmail.com>
2021-12-01 09:25:33 -0800

    better boilerplate

--- boilerplate.py
+++ boilerplate.py
@@ -1,8 +1,8 @@
-############### boilerplate ####################################################
 import os
 import sys
 import shutil
-from itertools import chain, combinations
+from itertools import *
+from collections import *
 from copy import deepcopy
 
 # change to dir of script
@@ -17,6 +17,21 @@ try:
 except:
     print("no " + input_file)
     data, lines = "", []
+
+
+def ans(answer):
+    # store answer to clipboard
+    from distutils.spawn import find_executable
+
+    xclip_path = find_executable("xclip")
+    if xclip_path:
+        os.system(f'echo "{answer}"| {xclip_path} -selection clipboard -in')
+        print("\t", answer, "| in clipboard\n")
+    else:
+        print(f"\t {answer} | (answer)\n")
+
+
+############### boilerplate ###################################################
 
 line_groups = data.split("\n\n")  # lines split by double newlines
 # line_groups = [l.strip() for l in line_groups]  # remove trailing newlines
@@ -36,18 +51,6 @@ def powerset(iterable):
     return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 
-def ans(answer):
-    # store answer to clipboard
-    from distutils.spawn import find_executable
-
-    xclip_path = find_executable("xclip")
-    if xclip_path:
-        os.system(f'echo "{answer}"| {xclip_path} -selection clipboard -in')
-        print("\t", answer, "| in clipboard\n")
-    else:
-        print(f"\t {answer} | (answer)\n")
-
-
 strips = lambda l: list(map(str.strip, l))
 ints = lambda l: list(map(int, l))
 commas = lambda s: s.split(",")
@@ -56,16 +59,37 @@ comma_ints = lambda s: ints(strips(s.split(",")))
 L, I, D, S = list, int, dict, set
 P, E, R, M = print, enumerate, range, map
 
-############### end of boilerplate #############################################
+############### end of boilerplate ############################################
+
+
+### PART 1 ###
 
 
 def line_transform(line):
+    "I run on each line of the input"
     # split = [line.split() for line in lines]
     # return int(line)
     return line
 
+lines = [line_transform(line) for line in lines]
 
-lines = [line_transform(line) for line in lines]  # apply line_transform to each line
+def part1(lines):
+    tot = 0
+    for idx, line in enumerate(lines):
+        if line:
+            tot += 1
+    return tot
 
-for idx, line in enumerate(lines):
+
+### PART 2 ###
+
+
+def part2(lines):
     pass
+
+
+if __name__ == "__main__":
+    p1_ans = part1(deepcopy(lines))
+    ans(p1_ans)
+    # p2_ans = part2(deepcopy(lines))
+    # ans(p2_ans)
