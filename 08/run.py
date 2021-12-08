dd9a799c4b5f2ae630f572e7fe9498b2d0f3ba91
Kevin Leutzinger <thedookmaster@gmail.com>
2021-12-07 22:26:28 -0800

    solved pt 2. dont feel like cleaning up this one

--- 08/run.py
+++ 08/run.py
@@ -3,6 +3,7 @@ import sys
 from collections import *
 from copy import deepcopy
 from itertools import *
+from pprint import pprint
 
 # change to dir of script
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -76,45 +77,126 @@ def line_transform(line):
     # return int(line)
     def to_sets(d):
         return [set(i) for i in d]
-    'cefbd dcg dcfgbae bdeafg gfcaed ecbgd bcag bdacge cg bedag | cebdg egcfda gdacbe badefg'
-    l, r = line.split(' | ')
-    l, r = to_sets(l.split(' ')), to_sets(r.split(' '))
+
+    "cefbd dcg dcfgbae bdeafg gfcaed ecbgd bcag bdacge cg bedag | cebdg egcfda gdacbe badefg"
+    l, r = line.split(" | ")
+    l, r = to_sets(l.split(" ")), to_sets(r.split(" "))
     return l, r
 
 
 lines = [line_transform(line) for line in lines]
 
 
-o = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
+o = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+
 
 def part1(data):
     tot = 0
     for d in data:
         for s in d:
-            if len(s) in [2,3,4,7]:
+            if len(s) in [2, 3, 4, 7]:
                 tot += 1
     return tot
     for d in data:
         if len(d) == 2:
             one = d
-            tot +=1
+            tot += 1
         if len(d) == 3:
             seven = d
-            tot +=1
+            tot += 1
         if len(d) == 4:
             four = d
-            tot +=1
+            tot += 1
         if len(d) == 7:
             eight = d
-            tot +=1
+            tot += 1
         print(d)
     return tot
-    
+
     tot = 0
     for idx, d in enumerate(data):
         if d:
             tot += 1
     return tot
+
+
+"""
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+
+"""
+
+
+def line_to_output(l, r):
+    """
+    2: one
+    3: seven
+    4: four
+    5: two, three, five
+    6: zero, six, nine
+    7: eight
+
+    """
+    sig = dict()
+
+    def go(num, d_):
+        nonlocal sig
+        if num in sig.keys():
+            pprint(sig)
+            print(f"{num=} already in sig")
+            exit(1)
+        sig[num] = d_
+
+    for d in l:
+        if len(d) == 2:
+            go(1, d)
+        if len(d) == 3:
+            go(7, d)
+        if len(d) == 4:
+            go(4, d)
+        if len(d) == 7:
+            go(8, d)
+    for d in l:
+        if len(d) == 6 and len(d - sig[1]) == 5:
+            go(6, d)
+    for d in l:
+        if len(d) == 6 and sig[4].issubset(d):
+            go(9, d)
+    for d in l:
+        if len(d) == 6 and d not in sig.values():
+            go(0, d)
+    for d in l:
+        if len(d) == 5 and d.issubset(sig[6]):
+            go(5, d)
+    for d in l:
+        if len(d) == 5 and sig[1].issubset(d) and d.issubset(sig[9]):
+            go(3, d)
+    for d in l:
+        if d not in sig.values():
+            go(2, d)
+
+    out = ""
+    for d in r:
+        for num, v in sig.items():
+            if d == v:
+                out += str(num)
+
+    return int(out)
 
 
 ### PART 2 ###
@@ -126,9 +208,8 @@ def part2(lines):
 
 if __name__ == "__main__":
     data = [l[1] for l in lines]
-    # not 200
-    for d in data:
-        print(len(d))
-    ans(part1(deepcopy(data)))
-    # p2_ans = part2(deepcopy(lines))
-    # ans(p2_ans)
+    ans(part1(deepcopy(data)))  # 274
+    tot = 0
+    for line in lines:
+        tot += line_to_output(*line)
+    ans(tot)  # 1012089
