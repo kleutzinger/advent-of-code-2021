75df6a422510084a6bab508ee794ef65834e6910
Kevin Leutzinger <thedookmaster@gmail.com>
2021-12-09 21:17:42 -0800

    solved pt 1 day 10

---
+++ 10/run.py
@@ -0,0 +1,137 @@
+import os
+import sys
+from collections import *
+from copy import deepcopy
+from itertools import *
+
+# change to dir of script
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+input_file = "input.txt"
+if "s" in sys.argv:
+    input_file = "input_small.txt"
+try:
+    with open(input_file) as f:
+        data = f.read()  # entire file as string
+        lines = data.splitlines()
+except:
+    print("no " + input_file)
+    data, lines = "", []
+
+
+def ans(answer):
+    # store answer to clipboard
+    from distutils.spawn import find_executable
+
+    xclip_path = find_executable("xclip")
+    if xclip_path:
+        os.system(f'echo "{answer}"| {xclip_path} -selection clipboard -in')
+        print("\t", answer, "| in clipboard\n")
+    else:
+        print(f"\t {answer} | (answer)\n")
+
+
+############### boilerplate ###################################################
+
+line_groups = data.split("\n\n")  # lines split by double newlines
+# line_groups = [l.strip() for l in line_groups]  # remove trailing newlines
+# print(lines)
+print(f"{len(lines)} lines in {input_file}\n")
+
+
+def coords(arr2d):
+    # return [(x0,y0), (x1, y0), ...]
+    for y in range(len(arr2d)):
+        for x in range(len(arr2d[y])):
+            yield (x, y)
+
+
+def rotate2d(l):
+    "rotate a 2d list counter_clockwise once"
+    nu = deepcopy(l)
+    return list(zip(*nu))[::-1]
+
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+
+strips = lambda l: list(map(str.strip, l))
+ints = lambda l: list(map(int, l))
+commas = lambda s: s.split(",")
+comma_ints = lambda s: ints(strips(s.split(",")))
+
+L, I, D, S = list, int, dict, set
+P, E, R, M = print, enumerate, range, map
+
+############### end of boilerplate ############################################
+
+
+### PART 1 ###
+
+
+def line_transform(line):
+    "I run on each line of the input"
+    # split = [line.split() for line in lines]
+    # return int(line)
+    return [c for c in line]
+
+
+lines = [line_transform(line) for line in lines]
+
+if len(lines):
+    l = lines[0]
+
+try:
+    nums = [int(i.strip()) for i in lines[0]]
+except:
+    pass
+
+
+def part1(data):
+    tot = 0
+    m = {
+        '}': '{',
+        ')': '(',
+        '>': '<',
+        ']': '['
+    }
+    points = {
+        ")": 3 ,
+        "]": 57,
+        "}": 1197,
+        ">": 25137,
+    }
+    starts = m.values()
+    ends = m.keys()
+    tot = 0
+    for idx, line in enumerate(data):
+        print(line)
+        stack = []
+        for c in line:
+            if c in ends:
+                top = stack.pop()
+                print(c, top)
+                if top != m[c]:
+                    tot += points[c]
+                    print(idx, 'error', c, top)
+                    print('brioke')
+                    break
+            else:
+                stack.append(c)
+        else:
+            print(idx, 'no error')
+    return tot
+
+
+### PART 2 ###
+
+
+def part2(lines):
+    pass
+
+
+if __name__ == "__main__":
+    ans(part1(deepcopy(lines)))
+    # p2_ans = part2(deepcopy(lines))
+    # ans(p2_ans)
